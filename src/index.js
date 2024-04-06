f33a62fe1e2f37b7a9cba66fa1396a7581389b8e
MaccSob <mcdarkdream1@gmail.com>
2024-04-07 00:47:55 +0200

    testing testing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+
 function openCity(evt, cityName) {
     // Declare all variables
     var i, tabcontent, tablinks;
@@ -18,5 +20,33 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
   }
-
   
+  let div = document.createElement('div');
+  div.id = 'About';
+  div.className = 'tabcontent';
+  
+  // create a new heading and add it to the div
+  let h1 = document.createElement('h1');
+  h1.textContent = 'The Goodest of the Restaurants';
+  div.appendChild(h1);
+
+
+  let h3 = document.createElement('h3');
+  h3.textContent = 'About our Restaurant:';
+  div.appendChild(h3);
+
+  let p = document.createElement('p');
+  p.className = 'border';
+  p.textContent = "Our restaurant has been funded by Kamil Toboła in the 1976, in Wrocław. As an avid traveler, who's visited more than 40 countries,and has eaten and enjoyed many famous cuisines. But it was the Georgian kitchen, who he loved and loves to this day, the most,Therefore, when he returned to his hometown, Wrocław, he wanted everyone to experience the beautiful and tasty food of Georgia.";
+  div.appendChild(p);
+
+  let img = document.createElement('img');
+  img.className = 'img1';
+  img.src = "flag-for-georgia-svgrepo-com.svg";
+  div.appendChild(img);
+
+
+
+
+  // add div to the document
+  document.body.appendChild(div);
